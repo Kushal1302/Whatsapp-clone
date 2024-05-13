d4c5ed8f6cfe8b90ab927196d6b094e80c439225
Kushal Kumar <101333638+Kushal1302@users.noreply.github.com>
2024-05-13 14:40:51 +0530

    Update index.js

--- client/src/constant/index.js
+++ client/src/constant/index.js
@@ -1,2 +1,2 @@
-// export const Base_Url = "https://whatsapp-clone-ol77.onrender.com"
-export const Base_Url = "http://localhost:8000"
+export const Base_Url = "https://whatsapp-clone-ol77.onrender.com"
+//export const Base_Url = "http://localhost:8000"
